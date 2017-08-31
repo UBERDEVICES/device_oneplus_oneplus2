ccffeeb64f964a1a5c0b4a46b0552036ec78378d
Seraph08 <d-vandenburgh@hvcc.edu>
2017-08-31 15:44:23 -0400

    fix base media paths

--- base.mk
+++ base.mk
@@ -836,8 +836,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/qcom/common/media/media_profiles.xml:system/etc/media_profiles.xml \
-    device/qcom/common/media/media_codecs.xml:system/etc/media_codecs.xml
+    device/oneplus/oneplus2/media/media_profiles.xml:system/etc/media_profiles.xml \
+    device/oneplus/oneplus2/media/media_codecs.xml:system/etc/media_codecs.xml
 
 ifeq ($(strip $(TARGET_USES_NQ_NFC)),true)
 PRODUCT_COPY_FILES += \
