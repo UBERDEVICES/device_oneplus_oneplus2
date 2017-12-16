b6ea53bbfeeb95d9966d51970c9df9bf67a76a2e
hardwareadictos <hardwareadictos@gmail.com>
2017-12-16 16:01:50 +0100

    Revert "oneplus2: disable camera for now"
    This reverts commit 139252001153227b84cf7aa430acee351e5a44fc.

--- device.mk
+++ device.mk
@@ -123,7 +123,7 @@ PRODUCT_PACKAGES += \
     libantradio
 
 # Camera
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     libshim_atomic \
