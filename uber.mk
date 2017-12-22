1e450a1ba456c247f76006c39bceb14b4c7fb7b6
RJ Trujillo <CertifiedBlyndGuy@gmail.com>
2017-12-22 16:55:47 +0100

    op2: Uber CAF bringup

--- uber.mk
+++ uber.mk
@@ -24,7 +24,7 @@ $(call inherit-product, device/oneplus/oneplus2/device.mk)
 # Inherit some common GZOSP stuff.
 $(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := gzosp_oneplus2
+PRODUCT_NAME := uber_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -37,7 +37,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus2 PRODUCT_NAME=OnePlus2
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=OnePlus/OnePlus2/OnePlus2:6.0.1/MMB29M/1447858500:user/release-keys \
-    PRIVATE_BUILD_DESC="OnePlus2-user 6.0.1 MMB29M 7 dev-keys" 
+    PRIVATE_BUILD_DESC="OnePlus2-user 6.0.1 MMB29M 7 dev-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
 
